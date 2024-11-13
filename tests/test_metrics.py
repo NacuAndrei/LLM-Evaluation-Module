f94f70352bebc3fee8e1d8b7f45b79934e475937
Andrei Nacu <andrei.nacu@publicissapient.com>
2024-11-13 12:55:48 +0200

    Add logging setup and dotenv support to test_metrics.py; create requirements.txt

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,4 +1,8 @@
 import os
+import sys
+import logging
+
+from dotenv import load_dotenv
 
 EVAL_DATASET_PATH = os.path.join(
     os.path.dirname(__file__), f"data/{os.environ.get('DATASET_FILENAME')}"
@@ -6,7 +10,13 @@ EVAL_DATASET_PATH = os.path.join(
 
 CHUNK_SIZES = {"small": 300, "medium": 650, "large": 1000}
 
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+
 def get_available_metrics():
     metrics = {}
     
     return metrics
+
