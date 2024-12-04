8aee8e36c3a7b0fe39f14db97adc9ac3881d9555
Andrei Nacu <andrei.nacu@publicissapient.com>
2024-12-04 14:59:35 +0200

    Update .gitignore and add chunk sizes to test_metrics.py

--- test_metrics.py
+++ test_metrics.py
@@ -24,6 +24,8 @@ load_dotenv()
 
 EVAL_DATASET_PATH = os.path.join(os.path.dirname(__file__), f"data/{os.environ.get('DATASET_FILENAME')}")
 
+CHUNK_SIZES = {"small": 300, "medium": 650, "large": 1000}
+
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger(__name__)
 
