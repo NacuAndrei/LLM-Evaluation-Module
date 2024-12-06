7e9832bdfc5b6d2ac389eaeb9ba541f34ea80500
Sorin Marian <sorin.marian@publicissapient.com>
2024-12-06 17:43:57 +0200

    Created a working Chain, but with versioning issues between Ragas and OpenAI; Removed unnecessary libraries and classes

--- requirements.py
+++ requirements.py
@@ -1,51 +1,60 @@
 PACKAGES = [
     "python-dotenv==1.0.1",
     "ruamel.yaml==0.18.6",
-    "datasets==3.0.1",
-
-    "ragas==0.1.21",
-    
-    "langchain==0.2.14",
-    "langchainhub==0.1.18",
-    "langchain-community==0.2.12",
-    "langchain-openai==0.1.8",
-    "langchain-pinecone==0.1.1",
-    "langchain-experimental==0.0.60",
-    "langchain-qdrant==0.1.0",
-    "langchain-cohere==0.1.7",
-
-    "unstructured==0.14.5",
-    "unstructured_pytesseract==0.3.12",
-    "unstructured_inference==0.7.34",
-    "unstructured[pptx]==0.14.5",
-
-    "pytesseract==0.3.10",
-    "paddleocr==2.7.3",
-    "pdfminer.six==20231228",
-    "pikepdf==8.15.1",
-    "pdf2image==1.17.0",
-    "pypdf==4.2.0",
-    "qrcode==7.4.2",
-    #"pycocotools==2.0.7",
-    "opencv-python-headless==4.9.0.80",
+    "datasets==3.0.1",    
     "openai==1.56.2",
-    "oci==2.131.1",
-    "pillow-heif==0.16.0",
-    "markdown==3.6",
-
-    "pyopenssl==24.1.0",
-    "python-docx==1.1.2",
-    "openpyxl==3.1.5",
+    "ragas==0.2.7",
     
-    "redis==5.0.7",
-    "upstash_redis==1.1.0",
-    "mysql-connector-python==9.0.0",
-    "psycopg2==2.9.9",
+    "faiss-cpu==1.8.0",
+    "beautifulsoup4",
 
-    "onnx==1.16.1",
-    "imgbeddings==0.1.0",
+
+    "langchain",
+    "langchainhub",
+    "langchain-community",
+    "langchain-openai",
+    "langchain-cohere",
     
-    "torch==2.3.1",
-    "transformers==4.41.2",
-    "huggingface_hub==0.24.7"
+    # "langchain==0.2.14",
+    # "langchainhub==0.1.18",
+    # "langchain-community==0.2.12",
+    # "langchain-openai==0.1.8",
+    # "langchain-pinecone==0.1.1",
+    # "langchain-experimental==0.0.60",
+    # "langchain-qdrant==0.1.0",
+    # "langchain-cohere==0.1.7",
+
+    # "unstructured==0.14.5",
+    # "unstructured_pytesseract==0.3.12",
+    # "unstructured_inference==0.7.34",
+    # "unstructured[pptx]==0.14.5",
+
+    # "pytesseract==0.3.10",
+    # "paddleocr==2.7.3",
+    # "pdfminer.six==20231228",
+    # "pikepdf==8.15.1",
+    # "pdf2image==1.17.0",
+    # "pypdf==4.2.0",
+    # "qrcode==7.4.2",
+    # "pycocotools==2.0.7",
+    # "opencv-python-headless==4.9.0.80",    
+    # "oci==2.131.1",
+    # "pillow-heif==0.16.0",
+    # "markdown==3.6",
+
+    # "pyopenssl==24.1.0",
+    # "python-docx==1.1.2",
+    # "openpyxl==3.1.5",
+    
+    # "redis==5.0.7",
+    # "upstash_redis==1.1.0",
+    # "mysql-connector-python==9.0.0",
+    # "psycopg2==2.9.9",
+
+    # "onnx==1.16.1",
+    # "imgbeddings==0.1.0",
+    
+    # "torch==2.3.1",
+    # "transformers==4.41.2",
+    # "huggingface_hub==0.24.7"
 ]
--- test_metrics.py
+++ test_metrics.py
@@ -1,28 +1,37 @@
+#General
 import os
 import sys
 import json
 import logging
 from typing import List, Dict, Any
-
 import unittest
 import pandas as pd
 from dotenv import load_dotenv
 from ruamel.yaml import YAML
 from datasets import Dataset
 
-from langchain_community.embeddings.openai import OpenAIEmbeddings
-from langchain.chains.combine_documents.stuff import StuffDocumentsChain
-from langchain_community.llms import OpenAIChat
-
-from ragas.metrics import faithfulness, answer_similarity, context_precision
+#Ragas
+from ragas.metrics import FactualCorrectness
 from ragas.run_config import RunConfig
 from ragas.llms import LangchainLLMWrapper
 from ragas.embeddings import LangchainEmbeddingsWrapper
-from ragas.metrics.base import MetricWithLLM, MetricWithEmbeddings
 from ragas import evaluate
-from langchain_core.output_parsers import StrOutputParser
 
-from custom_ragas_metric_prompt import CUSTOM_LONG_FORM_ANSWER_PROMPT
+#Embeddings & VectorStore
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+#LLM To be Evaluated
+from langchain_community.llms import OpenAIChat
+#from langchain_openai.chat_models import ChatOpenAI
+
+#Temporary
+from langchain import hub
+from langchain_community.document_loaders import WebBaseLoader
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
 
 load_dotenv()
 
@@ -36,80 +45,65 @@ class RagasEvaluator:
     def __init__(self, config: Dict[str, Any]) -> None:
         self.config = config
         
-        embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
         embeddings = OpenAIEmbeddings(model=self.config["embeddings"]["model"])
         self.embedding = LangchainEmbeddingsWrapper(embeddings)
+                
+        self.llm_to_be_evaluated = OpenAIChat(model=self.config["eval_llm"]["model"])            
 
-        llm = OpenAIChat(model=self.config["eval_llm"]["model"])
-        self.llm = LangchainLLMWrapper(llm)        
+        # See full prompt at https://smith.langchain.com/hub/rlm/rag-prompt
+        prompt = hub.pull("rlm/rag-prompt")
 
+        loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+        data = loader.load()
+
+        # Split text in chunks and store it in FAISS vectorstore
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+        all_splits = text_splitter.split_documents(data)
+        vectorstore = FAISS.from_documents(documents=all_splits, embedding=OpenAIEmbeddings())
+
+        #llm_to_be_evaluated Chain Creation
+        self.chain = (
+            {
+                "context": vectorstore.as_retriever() | self.format_docs,
+                "question": RunnablePassthrough(),
+            }
+            | prompt
+            | self.llm_to_be_evaluated
+            | StrOutputParser()
+        )
+
+        #Ragas LLM
+        self.llm = LangchainLLMWrapper(self.llm_to_be_evaluated)
         self.init_ragas_metrics()
-    
-    def get_available_metrics(self):
-        metrics = {
-            "faithfulness": faithfulness,
-            "answer_similarity": answer_similarity,
-            "context_precision": context_precision,
-        }
-        metrics["faithfulness"].statement_prompt = CUSTOM_LONG_FORM_ANSWER_PROMPT
-        return metrics
+
+    def format_docs(self, docs):
+        return "\n\n".join(doc.page_content for doc in docs)
 
     def init_ragas_metrics(self):
-        available_metrics = self.get_available_metrics()
         self.metrics = []
-        for name in self.config["test"]["metrics"]:
-            try:
-                self.metrics += [available_metrics[name]]
-            except AttributeError:
-                logger.warning(f"Skipping metric {name} - currently not available")
 
-        for metric in self.metrics:
-            if isinstance(metric, MetricWithLLM):
-                metric.llm = self.llm
-            if isinstance(metric, MetricWithEmbeddings):
-                metric.embeddings = self.embedding
-            run_config = RunConfig()
-            metric.init(run_config)
+        metric = FactualCorrectness() #TODO 2 Add more metrics
+        metric.llm = self.llm
+
+        run_config = RunConfig()
+        metric.init(run_config)
+
+        self.metrics += [metric]
             
     def load_dataset(self, absolute_path: str):
         with open(absolute_path, "r") as json_file:
-            all_examples = json.load(json_file)
-        return all_examples
+            all_samples = json.load(json_file)
+        return all_samples
     
-    def get_chunks_from_chain(self, response):
-        # text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-        # chunks = text_splitter.split_documents(documents)
-        chunks = response["sources"].split("\n\n\n")[1:]
-        for idx, chunk in enumerate(chunks):
-            chunks[idx] = chunk.split("is below.\n")[1]
-        logger.debug(f"Contexts: \n {chunks}")
-        return chunks
-
-    def score_with_ragas(self, query: str, ground_truth: str, chunks: List[str], answer: str):
-        scores = {}
-        for m in self.metrics:
-            logger.debug(f"calculating: {m.name}")
-            scores[m.name] = m.score(
-                row={
-                    "question": query,
-                    "ground_truth": ground_truth,
-                    "context": chunks,
-                    "answer": answer,
-                }
-            )
-            logger.info(f"{m.name} score: {scores[m.name]}")
-        return scores
-    
-    def get_complete_example(self, example: Dict[str, Any]):
-        question = example["question"]
+    def get_complete_sample(self, sample: Dict[str, Any]):
+        question = sample["question"]
         prompt = f"Please formulate the answer for the following question in one or multiple sentences. This is the question: {question}"
 
-        self.chain = prompt | self.llm | StrOutputParser()
-        response = self.chain.invoke(prompt)
+        response = self.chain.invoke(prompt) #TODO 1 find a working pair of versions for Ragas & OpenAI (Ragas 0.2.7 calls openai.ChatCompletion, but this is no longer supported in openai>=1.0.0; but ragas 0.2.7 depends on openai>1; ???)
         
-        example["model_answer"] = response["response"]                
+        sample["model_answer"] = response["response"]                
 
-        return example
+        return sample
 
     def get_evaluation_batch(self):
         evaluation_batch = {
@@ -139,21 +133,21 @@ class RagasEvaluator:
         #         doc.metadata["doc_type"] = doc_type
         #     documents.append(doc)
         
-        self.incomplete_examples = self.load_dataset(absolute_path=EVAL_DATASET_PATH)
+        self.incomplete_samples = self.load_dataset(absolute_path=EVAL_DATASET_PATH)
 
-        for incomplete_example in self.incomplete_examples:
-            complete_example = self.get_complete_example(incomplete_example)
+        for incomplete_sample in self.incomplete_samples:
+            complete_sample = self.get_complete_sample(incomplete_sample)
 
-            evaluation_batch["question"].append(complete_example["question"])
-            evaluation_batch["ground_truth"].append(complete_example["ground_truth"])
-            evaluation_batch["model_answer"].append(complete_example["model_answer"])
+            evaluation_batch["question"].append(complete_sample["question"])
+            evaluation_batch["ground_truth"].append(complete_sample["ground_truth"])
+            evaluation_batch["model_answer"].append(complete_sample["model_answer"])
 
         return evaluation_batch
     
     def write_results_to_excel(self, results):
         new_data = {
             "Doc format": [self.config["test"]["doc_format"]],
-            "# QA GT": [len(self.all_examples)],
+            "# QA GT": [len(self.all_samples)],
             "top_k": [self.chain_config["retriever"]["search_kwargs"]["k"]],
             "chunk_size": [CHUNK_SIZES[self.chain_config["ingestion"]["chunk_size"]]],
             "chunk_overlap": [self.chain_config["ingestion"]["overlap"]],
