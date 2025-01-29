0baba95e44d64a04c620bd5689b5e49ea2efd61f
Andrei Nacu <andrei.nacu@publicissapient.com>
2025-01-29 11:14:22 +0200

    Update LLM configuration in config.yml and enhance LangchainEvaluator to utilize the new LLM provider

--- langchain_evaluator.py
+++ langchain_evaluator.py
@@ -1,10 +1,14 @@
 import pandas as pd
 from langchain.evaluation import load_evaluator
+
 from llm_invoker import LLMInvoker
+from llm_provider import get_llm
 
 class LangchainEvaluator:
     def __init__(self, questions, config):
-        self.evaluator = load_evaluator("labeled_criteria", criteria="correctness")
+        self.llm_judge_config = config["llm_judge"]
+        
+        self.evaluator = load_evaluator("labeled_criteria", llm=get_llm(self.llm_judge_config, type="llm"), criteria="correctness")
         self.questions = questions
         self.config = config
     
